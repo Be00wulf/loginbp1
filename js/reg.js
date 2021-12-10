f14bb946811e0b7de3b56c3feb670a1737ff2e1b
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-12-09 22:33:51 -0600

    funciona

--- js/reg.js
+++ js/reg.js
@@ -5,18 +5,9 @@ const password2 = document.getElementById('password2');
 const invalid = document.getElementById('invalid')
 const form = document.querySelector('.form')
 
-/*var expresionesregulares = {
-    //email1: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    name1: /^[A-Z\s]{10,80}$/,
-    passwordexp: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,16}$/,
-}*/
 
 function validarRegistro(email, name, password1, password2) {
-    /*form.addEventListener('submit', (e) => e.preventDefault())
-    //let testemail = expresionesregulares.email1.test(email.value)
-    let testname = expresionesregulares.name1.test(name.value)
-    let testpassword = expresionesregulares.passwordexp.test(password1.value)
-    let testpassword2 = expresionesregulares.passwordexp.test(password2.value)*/
+
 
     if (testemail === true) {
         email.style.border = '1px solid hsl(0, 80%, 86%)'
